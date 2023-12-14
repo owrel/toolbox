ba9978084c092822656ed8d3b19f1501c50eb517
Owrel <aurelien.simon49@gmail.com>
2023-12-14 14:46:04 +0100

    Restructure

--- file/__init__.py
+++ file/__init__.py
@@ -1,4 +1,17 @@
-from .file import extract_filename
-
 
 __all__ = ["extract_filename"]
+
+from os import PathLike
+import re
+
+
+def extract_filename(path: PathLike):
+    x = 0
+    y = len(path)
+    if '/' in path:
+        x = path.rindex('/') + 1
+
+    if (m := re.search(r"\..*", path)):
+        y = m.span()[0]
+
+    return path[x:y]
